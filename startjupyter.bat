2ccbd52953bb93b26f369c71040509822c3f297a
llathrop <llathrop@gmail.com>
2017-09-14 14:59:09 -0500

    chore: add a script to start the jupyter session in win

---
+++ startjupyter.bat
@@ -0,0 +1,4 @@
+@echo on
+c:
+cd C:\Users\llathrop\Documents\projects\udacity-AIND\class_projects\aind2-cnn
+python "C:/ProgramData/Anaconda3/Scripts/jupyter-notebook-script.py" %USERPROFILE%\Documents\projects
